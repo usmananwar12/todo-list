663ec6243ab84959443c981fd4d9245d205b9997
Usman Anwar <usmananwar9957@gmail.com>
2025-05-04 18:35:59 +0500

    wo akhri nai tha lol

--- src/MyComponents/header.js
+++ src/MyComponents/header.js
@@ -14,7 +14,7 @@ export default function header(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="todo-list/#">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#About">About</a>
